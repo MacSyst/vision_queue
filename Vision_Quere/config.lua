d0884a9e21e19fe1bd3a92179bcacd18e2d152dd
Vision-Scripts <167629435+MacSyst@users.noreply.github.com>
2024-04-20 18:43:47 +0200

    Update config.lua

--- Vision_Quere/config.lua
+++ Vision_Quere/config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
 Config.startup = false
-Config.webhook = "https://discord.com/api/webhooks/1226958828508287117/sKM_ORXd03tYuZ_W7Gp8q4Vfpmy1Z1kXRs9wFLo5krUrSkdY5LbHWFC64BBYebIwYa8G"
+Config.webhook = ""
 Config.queueTime = 5	
 Config.maxPlayers = 100
